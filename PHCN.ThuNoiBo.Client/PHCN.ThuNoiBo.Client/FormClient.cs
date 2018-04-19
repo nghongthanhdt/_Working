7cc184d12fa681bbc87ec541914e5921c474f4f7
Nguyen Hong Thanh <ng.hong.thanh@gmail.com>
2018-04-19 21:54:54 +0700

    19/04/2018

--- PHCN.ThuNoiBo.Client/PHCN.ThuNoiBo.Client/FormClient.cs
+++ PHCN.ThuNoiBo.Client/PHCN.ThuNoiBo.Client/FormClient.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace PHCN.ThuNoiBo.Client
 {
+
+
+
     public partial class FormClient : DevExpress.XtraEditors.XtraForm
     {
+
+
+
         public FormClient()
         {
             InitializeComponent();
@@ -26,42 +34,75 @@ namespace PHCN.ThuNoiBo.Client
 
         }
 
-        private void FormClient_Resize(object sender, EventArgs e)
-        {
-            if (FormWindowState.Minimized == this.WindowState)
-            {
-                notifyIconMain.Visible = true;
-                notifyIconMain.BalloonTipTitle = "Bệnh viện Phục hồi chức năng Đồng Tháp";
-                notifyIconMain.BalloonTipText = "Hệ thống thư nội bộ";
-                notifyIconMain.ShowBalloonTip(500);
-                this.Hide();
-            }
-            else if (FormWindowState.Normal == this.WindowState)
-            {
-                notifyIconMain.Visible = false;
-                this.Show();
-                this.Activate();
-            }
-
-        }
-
 
 
         private void notifyIconMain_DoubleClick(object sender, EventArgs e)
         {
-            // Show the form when the user double clicks on the notify icon.
 
-            // Set the WindowState to normal if the form is minimized.
-            if (this.WindowState == FormWindowState.Minimized)
-                this.WindowState = FormWindowState.Normal;
 
-            // Activate the form.
-            this.Activate();
+
+            
         }
 
         private void FormClient_FormClosed(object sender, FormClosedEventArgs e)
         {
 
+        }
+
+        private void FormClient_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notifyIconMain_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // Activate the form.
+            this.Show();
+
+
+            
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void FormClient_VisibleChanged(object sender, EventArgs e)
+        {
+
+            if (this.Visible != true)
+            {
+                notifyIconMain.Visible = true;
+                notifyIconMain.BalloonTipTitle = "Bệnh viện Phục hồi chức năng Đồng Tháp";
+                notifyIconMain.BalloonTipText = "Hệ thống thư nội bộ";
+                notifyIconMain.ShowBalloonTip(500);
+            }
+            
+
+
+        }
+
+        private void btnCapNhatCauHinh_Click(object sender, EventArgs e)
+        {
+
+            ClientConfig clientConfig = new ClientConfig();
+            clientConfig.ConnectServer = txtConnectServer.Text;
+            clientConfig.ConnectUserName = txtConnectUSerName.Text;
+            clientConfig.ConnectPassword = txtConnectPassword.Text;
+            clientConfig.AccountUserName = txtAccountUserName.Text;
+            clientConfig.AccountPassword = txtAccountPassword.Text;
+            clientConfig.AutoStart = checkboxShowOnStart.Checked;
+            clientConfig.ShowOnStart = checkboxShowOnStart.Checked;
+            clientConfig.AutoGetMail = checkboxAutoGetMail.Checked;
+            clientConfig.AutoGetMailTimer = int.Parse(txtAutoGetMailTimer.Text);
+
+            // kết nối máy chủ
+            // kiểm tra tài khoản
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ClientConfig));
+            serializer.Serialize(File.Create("config.xml"), clientConfig);
+            
         }
     }
 }
