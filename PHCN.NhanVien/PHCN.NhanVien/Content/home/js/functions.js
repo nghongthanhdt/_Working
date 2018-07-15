2b123d402087852cffa9f8ff2a376ad8fef7ba7b
Nguyễn Hồng Thanh <ng.hong.thanh@gmail.com>
2018-07-15 21:52:32 +0700

    15072018

--- PHCN.NhanVien/PHCN.NhanVien/Content/home/js/functions.js
+++ PHCN.NhanVien/PHCN.NhanVien/Content/home/js/functions.js
@@ -1,20 +1,17 @@
-(function ($) {
+jQuery(function($) {
+
+	//#main-slider
+	$(function(){
+		$('#main-slider.carousel').carousel({
+			interval: 8000
+		});
+	});
 	
-		var slider = $('.bxslider').bxSlider({
-  mode: 'horizontal'
-});
-
-$('#reload-slider').click(function(e){
-  e.preventDefault();
-  slider.reloadSlider({
-    mode: 'fade',
-    auto: true,
-    pause: 1000,
-    speed: 500
-  });
-});
-
-$(window).scroll(function(){
+	
+	//Initiat WOW JS
+	new WOW().init();
+	
+	$(window).scroll(function(){
 		if ($(this).scrollTop() > 100) {
 			$('.scrollup').fadeIn();
 			} else {
@@ -25,14 +22,47 @@ $(window).scroll(function(){
 			$("html, body").animate({ scrollTop: 0 }, 1000);
 				return false;
 		});
-		
-		wow = new WOW(
-	 {
 	
-		}	) 
-		.init();
+	// portfolio filter
+	$(window).load(function(){'use strict';
+		var $portfolio_selectors = $('.portfolio-filter >li>a');
+		var $portfolio = $('.portfolio-items');
+		$portfolio.isotope({
+			itemSelector : '.portfolio-item',
+			layoutMode : 'fitRows'
+		});
 		
-})(jQuery);
+		$portfolio_selectors.on('click', function(){
+			$portfolio_selectors.removeClass('active');
+			$(this).addClass('active');
+			var selector = $(this).attr('data-filter');
+			$portfolio.isotope({ filter: selector });
+			return false;
+		});
+	});
 
 
+	//Pretty Photo
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});	
+    
+    //Google Map
+    var get_latitude = $('#google-map').data('latitude');
+    var get_longitude = $('#google-map').data('longitude');
 
+    function initialize_google_map() {
+        var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
+        var mapOptions = {
+            zoom: 14,
+            scrollwheel: false,
+            center: myLatlng
+        };
+        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize_google_map);
+});
---
+++ PHCN.NhanVien/PHCN.NhanVien/Controllers/QuanLyBaiVietController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PHCN.NhanVien.Controllers
+{
+    public class QuanLyBaiVietController : Controller
+    {
+        // GET: QuanLyBaiViet
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult _pDanhSachBaiViet()
+        {
+            return View();
+        }
+    }
+}
