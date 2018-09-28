51c24919a6454783a3380af5a2bd89770ee5b462
Nguyễn Hồng Thanh <ng.hong.thanh@gmail.com>
2018-09-28 18:55:55 +0700

    28/09/2018

---
+++ PHCN.NhanVien/PHCN.NhanVien/Controllers/QuanLySucKhoeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PHCN.NhanVien.Controllers
+{
+    public class QuanLySucKhoeController : Controller
+    {
+        // GET: QuanLySucKhoe
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Xem()
+        {
+            return View();
+        }
+        public ActionResult LuotKham()
+        {
+            return View();
+        }
+        public ActionResult _pThongTinChung()
+        {
+            return View();
+        }
+        public ActionResult _pKhamSucKhoeDinhKy()
+        {
+            return View();
+        }
+    }
+}
